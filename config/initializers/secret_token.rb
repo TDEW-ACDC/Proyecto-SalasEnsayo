7d86db1c983786eaca91ecb667d29d6a650aa670
Daniel Fujimoto Gamero <daniel.fujimotog@gmail.com>
2012-03-17 13:01:36 -0500

    commit1-DF

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProyectoSalasensayo::Application.config.secret_token = '37b6a06fc95cb905ca743f19351d6593a5afbb90ad6b875a73dc08c83cc1ab87f4b1141e05a964214a9193196b41475c030ba473a9dd898ca25dfc0143b93a9e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ProyectoSalasensayo::Application.config.session_store :cookie_store, key: '_Proyecto-SalasEnsayo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ProyectoSalasensayo::Application.config.session_store :active_record_store
